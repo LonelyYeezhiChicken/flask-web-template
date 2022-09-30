0ddccbcf6bd7f130f5f182426c15aa1035fdda53
chicken <hibary.taiwan@gmail.com>
2022-09-30 15:07:55 +0800

    feat: google 翻譯模組

--- app/repository/translateRepository.py
+++ app/repository/translateRepository.py
@@ -1,16 +1,18 @@
-from translate import Translator
+import googletrans
+
+# Initial
+translator = googletrans.Translator()
 
 
 class TranslateRepository:
 
     def translate_content_ch(inData):
-      # 實現英文轉中文
-        translator = Translator(to_lang='zh-tw')
-        translation = translator.translate(inData)
-        return translation
+        # 實現英文轉中文
+        translation = translator.translate(inData, dest='zh-tw')
+        return translation.text
 
     def translate_content_en(inData):
         # 實現中文轉英文
-        translator = Translator(from_lang='zh-tw', to_lang='english')
-        translation = translator.translate(inData)
-        return translation
+        translator = translator(from_lang='zh-tw', to_lang='english')
+        translation = translator.translate(inData, dest="en")
+        return translation.text
